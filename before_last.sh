b3a27188491921ead3cf243958a27fcd4fe1780b
Chris D <chris.dehaene@gmail.com>
2025-11-14 14:20:45 +0100

    before_last.sh put into a before_last() function

--- before_last.sh
+++ before_last.sh
@@ -10,13 +10,18 @@ log() {
     echo -e "$BASENAME_SCRIPT, $1"
 }
 
-# if there is a $1 then overwrite FROM_LAST with it
-#check if there is a $1 given as input
-if [ ! "$1" == "" ] ; then
-    FROM_LAST=$1
-fi
+before_last() {
 
-# "ls -1tr" will return a list with only the filenames (-1) sorted by modified time (-t) in reverse order (-r)
-# tail & head usage: "tail -n 2" will return the last 2 lines. With "head -n 1" the single top line of these two is returned.
-# use quotes "" around the $() to still return as one string even if it contains spaces
-echo "$(ls -1tr | tail -n $FROM_LAST | head -n 1)"
+    # if there is a $1 then overwrite FROM_LAST with it
+    #check if there is a $1 given as input
+    if [ ! "$1" == "" ] ; then
+        FROM_LAST=$1
+    fi
+
+    # "ls -1tr" will return a list with only the filenames (-1) sorted by modified time (-t) in reverse order (-r)
+    # tail & head usage: "tail -n 2" will return the last 2 lines. With "head -n 1" the single top line of these two is returned.
+    # use quotes "" around the $() to still return as one string even if it contains spaces
+    echo "$(ls -1tr | tail -n $FROM_LAST | head -n 1)"
+}
+
+before_last $1
